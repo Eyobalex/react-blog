a797ed5c5014d7997dca25d1c03be3fd8ee841d5
Eyobalex <eyobalemayehu007@gmail.com>
2019-08-30 19:24:46 +0300

    initial commit

---
+++ src/build/precache-manifest.dc46941e0817652d16f2c4d5b83dd20b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3423549e76ed9bdc1bdbb2ef5ec57f78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "233df75a4da61f325381",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "f33b0b2e1f78eee1ea5b",
+    "url": "/static/js/2.7ff38613.chunk.js"
+  },
+  {
+    "revision": "233df75a4da61f325381",
+    "url": "/static/js/main.3a291764.chunk.js"
+  },
+  {
+    "revision": "1bb9cda2ac052721e899",
+    "url": "/static/js/runtime~main.923c2dd4.js"
+  }
+]);
